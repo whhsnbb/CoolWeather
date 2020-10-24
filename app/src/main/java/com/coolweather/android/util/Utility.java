886cf886b375a0eabc08b059dc61ca1e8d241089
“whhsnbb” <1144320036@qq.com>
2020-10-24 11:12:39 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,16 +5,18 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Utility {
+
     /**
      * 解析和处理服务器返回的省级数据
      */
-
     public static boolean handleProvinceResponse(String response){
         if(!TextUtils.isEmpty(response)){
             try{
@@ -38,8 +40,6 @@ public class Utility {
     /**
      * 解析和处理服务器返回的市级数据
      */
-
-
     public static boolean handleCityResponse(String response , int provinceId){
         if(!TextUtils.isEmpty(response)){
             try{
@@ -63,7 +63,6 @@ public class Utility {
     /**
      * 解析和处理服务器返回的县级数据
      */
-
     public static boolean handleCountyResponse(String response , int cityId){
         if(!TextUtils.isEmpty(response)){
             try{
@@ -82,5 +81,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
